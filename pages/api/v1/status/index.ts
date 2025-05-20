4a5f1a169795ad13ffb67d81d506fd64dc9f8a90
Ryan Gwalchmei <ryan.gwalchmei@outlook.com.br>
2025-05-20 13:30:33 -0300

    feat: adds `database` resource to use with `docker compose`

---
+++ pages/api/v1/status/index.ts
@@ -0,0 +1,11 @@
+import database from "infra/database";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1+1 as sum;");
+
+  console.log(result.rows);
+
+  res.status(200).json(result.rows);
+}
+
+export default status;
