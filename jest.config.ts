4fb9126de857938abf1e8991e82508dd7a0eac73
Ryan Gwalchmei <ryan.gwalchmei@outlook.com.br>
2025-05-20 10:38:28 -0300

    chore: create `jest.config.js`

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import nextJest from 'next/jest'
+const createJestConfig = nextJest({
+  dir: '.',
+})
+
+const jestConfig = {
+  moduleDirectories: ["node_modules", "<rootDir>"], //@ts-ignore
+  testTimeout: 60000,
+}
+
+export default createJestConfig(jestConfig)
