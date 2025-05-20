1641c89dd8a251438e9205588a09e3b74aa74c0f
Ryan Gwalchmei <ryan.gwalchmei@outlook.com.br>
2025-05-20 15:02:12 -0300

    feat: adds `InternalServerError` and `ServiceError` errors customizeds

---
+++ infra/errors/customizeds.ts
@@ -0,0 +1,47 @@
+export class InternalServerError extends Error {
+  action: string;
+  statusCode: number;
+
+  constructor({ cause, statusCode }: { cause: string; statusCode: number }) {
+    super("Um erro interno não esperado aconteceu.", {
+      cause,
+    });
+
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o suporte";
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  statusCode: number;
+  action: string;
+
+  constructor({ cause, message }: { cause: Error; message: string }) {
+    super(message || "Serviço indisponível no momento.", {
+      cause,
+    });
+
+    this.name = "ServiceError";
+    this.action = "Verifique se o serviço está disponível.";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
